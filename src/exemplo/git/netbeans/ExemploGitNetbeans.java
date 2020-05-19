00e242479b9d18f92fdc5acd1cb515d4d49f042a
Rein4ldoAlv3s <65620874+Rein4ldoAlv3s@users.noreply.github.com>
2020-05-19 15:30:23 -0300

    Update ExemploGitNetbeans.java

--- src/exemplo/git/netbeans/ExemploGitNetbeans.java
+++ src/exemplo/git/netbeans/ExemploGitNetbeans.java
@@ -3,9 +3,20 @@ package exemplo.git.netbeans;
 public class ExemploGitNetbeans {
 
     public static void main(String[] args) {
+        Scanne entrada = new Scanner(System.in);
         System.out.println("Exemplo de um projeto qualquer com git e netbeans");
         //alterado da web
-        //segunda alteracao por pelo netbeans
+        //segunda alteracao pelo netbeans
+        System.out.println("Hello World");
+        
+        //numero par ou impar?
+        int num=entrada.nextInt();
+        if(num%2==0){
+            System.out.println("Numero par");
+        }
+        else{
+            System.out.println("Numero impar");   
+        }
     }
     
 }
