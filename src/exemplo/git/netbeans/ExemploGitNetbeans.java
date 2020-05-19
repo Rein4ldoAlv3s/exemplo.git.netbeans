488ae609049c6d97a0bbdec9cbe494c1b8fb168d
reinaldo <reinaldo@reinaldo-desktop>
2020-05-19 15:03:09 -0300

    my first commit on netbeans(Reinaldo)

---
+++ src/exemplo/git/netbeans/ExemploGitNetbeans.java
@@ -0,0 +1,9 @@
+package exemplo.git.netbeans;
+
+public class ExemploGitNetbeans {
+
+    public static void main(String[] args) {
+        System.out.println("Exemplo de um projeto qualquer com git e netbeans");
+    }
+    
+}
