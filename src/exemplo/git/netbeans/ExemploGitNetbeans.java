2a027a3a7c987c634f27abde8a13a8c917c8edf6
Rein4ldoAlv3s <65620874+Rein4ldoAlv3s@users.noreply.github.com>
2020-05-19 15:25:48 -0300

    Update ExemploGitNetbeans.java

--- src/exemplo/git/netbeans/ExemploGitNetbeans.java
+++ src/exemplo/git/netbeans/ExemploGitNetbeans.java
@@ -5,6 +5,7 @@ public class ExemploGitNetbeans {
     public static void main(String[] args) {
         System.out.println("Exemplo de um projeto qualquer com git e netbeans");
         //alterado da web
+        //segunda alteracao por pelo netbeans
     }
     
 }
